e6ee73e06070dc946fd67545131d905bdfdb4e74
dawitbelayalemneh <dawitbelay27@gmail.com>
2026-03-20 00:51:40 +0300

    feat: added import export function

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Desktop, Laptop, Mobile
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 @admin.register(Desktop, Laptop, Mobile)
-class viewAdmin(admin.ModelAdmin):
+class viewAdmin(ImportExportModelAdmin):
     pass
